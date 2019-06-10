2d2f968db50da4aa8318d31744e2418fc64de4d4
KenshinTang <28135055@qq.com>
2019-06-10 15:13:22 +0800

    init ui

--- HashGenerator/ui/MainWindow.py
+++ HashGenerator/ui/MainWindow.py
@@ -9,10 +9,13 @@ XulDebugTool
 author: Kenshin
 last edited: 2019.06.10
 """
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import Qt
+
+import os
+from PyQt5.QtWidgets import QApplication, QLabel, QLineEdit, QPushButton, QFileDialog
+from PyQt5.QtCore import Qt, pyqtSlot
 
 from HashGenerator.ui.BaseWindow import BaseWindow
+from HashGenerator.ui.widgets.ClickableLineEdit import ClickableLineEdit
 
 
 class MainWindow(BaseWindow):
@@ -23,9 +26,48 @@ class MainWindow(BaseWindow):
 
     def initUI(self):
         self.setWindowFlags(Qt.WindowCloseButtonHint)
-        self.setFixedSize(460, 150)
+        self.setFixedSize(460, 160)
+
+        self.targetDir = QLabel(self)
+        self.targetDir.setText("Target Dir:")
+        self.targetDir.move(20, 28)
+
+        self.targetDirLineEdit = ClickableLineEdit(self)
+        self.targetDirLineEdit.resize(260, 28)
+        self.targetDirLineEdit.move(130, 28)
+        self.targetDirLineEdit.setReadOnly(True)
+        self.targetDirLineEdit.clicked.connect(self.onTargetClicked)
+
+        self.outputExcel = QLabel(self)
+        self.outputExcel.setText("Output Excel:")
+        self.outputExcel.move(20, 68)
+
+        self.outputExcelLineEdit = ClickableLineEdit(self)
+        self.outputExcelLineEdit.resize(260, 28)
+        self.outputExcelLineEdit.move(130, 68)
+        self.outputExcelLineEdit.setReadOnly(True)
+        self.outputExcelLineEdit.clicked.connect(self.onOutputClicked)
+
+        self.generatorButton = QPushButton(self)
+        self.generatorButton.setText("Generator")
+        self.generatorButton.move(180, 110)
+        self.generatorButton.resize(100, 28)
 
         # self.initMenuBar()
         # self.initLayout()
         super().initWindow()
 
+    @pyqtSlot()
+    def onTargetClicked(self):
+        self.dir = QFileDialog.getExistingDirectory(self, "选取文件夹", os.getcwd())
+        if self.dir != "":
+            self.targetDirLineEdit.setText(self.dir)
+
+    @pyqtSlot()
+    def onOutputClicked(self):
+        self.fileName, self.fileType = QFileDialog.getOpenFileName(self,
+                                                          "选取文件",
+                                                          os.getcwd(),
+                                                          "Excel File (*.xlsx)")
+        if self.fileName != "":
+            self.outputExcelLineEdit.setText(self.fileName)
---
+++ HashGenerator/ui/widgets/ClickableLineEdit.py
@@ -0,0 +1,11 @@
+from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtWidgets import QLineEdit
+
+
+class ClickableLineEdit(QLineEdit):
+    clicked = pyqtSignal() # signal when the text entry is left clicked
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton: self.clicked.emit()
+        else: super().mousePressEvent(event)
+
